26afc4f6a5979d2d1951ca3e748f4c61721748c4
Vedang <vedangsharma52@gmail.com>
2026-09-24 13:34:59 +0530

    Add Zoho OAuth callback endpoint

---
+++ api/integrations/zoho/callback.js
@@ -0,0 +1,10 @@
+module.exports = async function handler(request, response) {
+  const { handleAccountingRequest } = await import('../../../automation/accounting-routes.mjs');
+  if (request.method !== 'GET') {
+    response.setHeader('Allow', 'GET');
+    return response.status(405).json({ error: 'GET required' });
+  }
+  const incoming = new URL(request.url, 'https://cetld.invalid');
+  incoming.searchParams.set('provider', 'zoho_books');
+  return handleAccountingRequest({ ...request, url: incoming.pathname + incoming.search }, response);
+};
